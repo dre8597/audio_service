d64dace6415433ef7407bb598f7479010779ea13
Demondre Livingston <livingstondemondre7@gmail.com>
2023-01-16 09:55:05 -0500

    Clean up

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -29,38 +29,17 @@ public class AudioServiceActivity extends FlutterActivity {
         if (intent.getAction().compareTo(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH) == 0) {
             String mediaFocus = intent.getStringExtra(MediaStore.EXTRA_MEDIA_FOCUS);
             String query = intent.getStringExtra(SearchManager.QUERY);
-            // Some of these extras may not be available depending on the search mode
-            @Nullable String album = intent.getStringExtra(MediaStore.EXTRA_MEDIA_ALBUM);
-            @Nullable String artist = intent.getStringExtra(MediaStore.EXTRA_MEDIA_ARTIST);
-            @Nullable String genre = intent.getStringExtra("android.intent.extra.genre");
-            @Nullable String playlist = intent.getStringExtra("android.intent.extra.playlist");
-            @Nullable String title = intent.getStringExtra(MediaStore.EXTRA_MEDIA_TITLE);
-            // Determine the search mode and use the corresponding extras
-
 
             if (mediaFocus == null) {
                 // 'Unstructured' search mode (backward compatible)
-                listener.onPlayFromSearch(query, savedInstanceState);
-            } else if (mediaFocus.compareTo("vnd.android.cursor.item/*") == 0) {
+                listener.onPlayFromSearch(query, intent.getExtras());
+            } else {
                 if (!query.isEmpty()) {
                     // 'Unstructured' search mode
-                    listener.onPlayFromSearch(query, savedInstanceState);
+                    listener.onPlayFromSearch(query, intent.getExtras());
                 } else {
                     listener.onPlay();
                 }
-            } else if (
-                    mediaFocus.compareTo(MediaStore.Audio.Genres.ENTRY_CONTENT_TYPE) == 0) {
-                // 'Genre' search mode
-                listener.onPlayFromSearch(query, savedInstanceState);
-            } else if (mediaFocus.compareTo(MediaStore.Audio.Artists.ENTRY_CONTENT_TYPE) == 0) {
-                // 'Artist' search mode
-                listener.onPlayFromSearch(query, savedInstanceState);
-            } else if (mediaFocus.compareTo(MediaStore.Audio.Albums.ENTRY_CONTENT_TYPE) == 0) {
-                // 'Album' search mode
-                listener.onPlayFromSearch(query, savedInstanceState);
-            } else if (mediaFocus.compareTo("vnd.android.cursor.item/audio") == 0) {
-                // 'Song' search mode
-                listener.onPlayFromSearch(query, savedInstanceState);
             }
         }
     }
--- audio_service/android/src/main/java/com/ryanheise/audioservice/MediaButtonReceiver.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/MediaButtonReceiver.java
@@ -2,7 +2,6 @@ package com.ryanheise.audioservice;
 
 import android.content.Context;
 import android.content.Intent;
-import android.provider.MediaStore;
 
 public class MediaButtonReceiver extends androidx.media.session.MediaButtonReceiver {
     public static final String ACTION_NOTIFICATION_DELETE = "com.ryanheise.audioservice.intent.action.ACTION_NOTIFICATION_DELETE";
@@ -15,12 +14,7 @@ public class MediaButtonReceiver extends androidx.media.session.MediaButtonRecei
             AudioService.instance.handleDeleteNotification();
             return;
         }
-        // Added as an attempt to enable searching by voice using Google Assistant.
-        if (intent != null && intent.getAction().equals(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH)) {
-            String query = intent.getStringExtra(MediaStore.EXTRA_MEDIA_FOCUS);
-            AudioService.listener.onPlayFromSearch(query, intent.getExtras());
-            return;
-        }
+
         super.onReceive(context, intent);
     }
 }
